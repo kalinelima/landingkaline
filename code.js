fd94f55d49e3e88a61d3323acfc6086b3b7179b1
Kaline Nunes <kalinelima787@gmail.com>
2023-05-29 08:44:35 -0300

    versao1

--- code.js
+++ code.js
@@ -1,13 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu";
-    }
-  }
-  
-
 var btn = document.querySelector("#voltartopo");
   btn.addEventListener("click", function() {
       window.scrollTo(0, 0);
